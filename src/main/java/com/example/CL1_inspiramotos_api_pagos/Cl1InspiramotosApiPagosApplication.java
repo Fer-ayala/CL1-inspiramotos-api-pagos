c3d4b73c4cd732c1df4732b08f5df11a9a73fde6
Fernando <monasoxx@gmail.com>
2024-09-27 22:00:51 -0500

    iniciando api pagos

---
+++ src/main/java/com/example/CL1_inspiramotos_api_pagos/Cl1InspiramotosApiPagosApplication.java
@@ -0,0 +1,13 @@
+package com.example.CL1_inspiramotos_api_pagos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl1InspiramotosApiPagosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl1InspiramotosApiPagosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CL1_inspiramotos_api_pagos/Cl1InspiramotosApiPagosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CL1_inspiramotos_api_pagos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl1InspiramotosApiPagosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
